aaed6a87fa99242399f1e048a0b340f0c48e769e
Leonardo Steffen <steffencrespo@hotmail.com>
2015-08-20 20:26:45 -0300

    modifies basic test to now assert for json_body attribute

--- spec/basic_api_flow_spec.rb
+++ spec/basic_api_flow_spec.rb
@@ -3,6 +3,8 @@ require 'airborne'
 describe 'sample spec' do
   it 'should validate get' do
     get 'http://localhost:3000/api/flights'
+    expect(json_body[0][:data_partida]).to eq("2015-10-10T00:00:00.000Z")
+
     # expect_json_types(name: :string)
   end
 
