6c9b786c530914a2a268da7502dd66415f38d39a
Leonardo Steffen <steffencrespo@hotmail.com>
2015-08-24 18:33:32 -0300

    adds before each and after each

--- spec/basic_api_flow_spec.rb
+++ spec/basic_api_flow_spec.rb
@@ -1,8 +1,19 @@
 require 'airborne'
 
 describe 'sample spec' do
+
+before :each do
+	puts "inicia"
+end
+
+after :each do
+	puts "finaliza"
+end
+
+
   it 'should validate get' do
     get 'http://localhost:3000/api/flights'
+    puts "roda teste 1"
     expect(json_body[0][:data_partida]).to eq("2015-10-10T00:00:00.000Z")
 
     # expect_json_types(name: :string)
